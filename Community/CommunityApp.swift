40f7a5cf035802f548790609814096fc20998337
Darie-Nistor Nicolae <darie.nistornicolae@coolblue.nl>
2024-12-06 16:15:23 +0100

    Initial Commit

---
+++ Community/CommunityApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommunityApp.swift
+//  Community
+//
+//  Created by Darie-Nistor Nicolae on 06.12.2024.
+//
+
+import SwiftUI
+
+@main
+struct CommunityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
