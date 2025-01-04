8ffec6e15b6ae8b9707c93bee18ef2b54dcce1ee
Darie-Nistor Nicolae <darie.nistornicolae@coolblue.nl>
2025-01-04 18:49:15 +0100

    [MA-040125] Create add post button

--- Community/Views/HomeView.swift
+++ Community/Views/HomeView.swift
@@ -2,7 +2,18 @@ import SwiftUI
 
 struct HomeView: View {
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    ZStack {
+      ScrollView {
+
+      }
+      VStack {
+        Spacer()
+        HStack {
+          Spacer()
+          FloatingActionButton()
+        }
+      }
+    }
   }
 }
 
