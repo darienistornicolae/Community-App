0cfaafe88af680e4c3951ccdf0fbed65762db064
Darie-Nistor Nicolae <darie.nistornicolae@coolblue.nl>
2025-01-05 15:25:06 +0100

    [MA-040125] Update HomeView

--- Community/Views/HomeView.swift
+++ Community/Views/HomeView.swift
@@ -6,34 +6,7 @@ struct HomeView: View {
 
   var body: some View {
     ZStack {
-      ScrollView {
-        VStack(spacing: 20) {
-          // Points Display
-          HStack {
-            Spacer()
-            VStack {
-              HStack {
-                Image(systemName: "star.circle.fill")
-                  .foregroundColor(.yellow)
-                Text("\(viewModel.currentPoints)")
-                  .font(.title)
-                  .bold()
-              }
-              Text("POINTS")
-                .font(.caption)
-                .foregroundColor(.gray)
-            }
-            .padding()
-            .background(colorScheme == .dark ? Color(.systemGray6) : .white)
-            .cornerRadius(15)
-            .shadow(radius: 2)
-            Spacer()
-          }
-          .padding(.top)
-          
-          Spacer()
-        }
-      }
+      ScrollView {}
       VStack {
         Spacer()
         HStack {
