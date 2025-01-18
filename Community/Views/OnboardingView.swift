76e312f0cac86a8150d765d7a09940e3fb06ede4
Bart Steer <bart.steer@hotmail.com>
2025-01-18 12:19:16 +0100

    Added the complete onboarding view function, also added 6 images to the assets

---
+++ Community/Views/OnboardingView.swift
@@ -0,0 +1,64 @@
+import SwiftUI
+
+struct OnboardingView: View {
+    let onCompletion: () -> Void
+
+    @StateObject private var viewModel = OnboardingViewModel()
+
+    var body: some View {
+            VStack {
+                ProgressView(value: Double(viewModel.currentStepIndex + 1), total: Double(viewModel.steps.count))
+                    .progressViewStyle(LinearProgressViewStyle())
+                    .padding(.top, 20)
+                    .padding(.horizontal)
+                
+                Spacer()
+                
+                Text(viewModel.steps[viewModel.currentStepIndex].title)
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding()
+
+                if let imageName = viewModel.steps[viewModel.currentStepIndex].image {
+                    Image(imageName)
+                            .resizable()
+                            .frame(width: 300, height: 200)
+                            .clipped()
+                            .padding()
+                }
+
+                Text(viewModel.steps[viewModel.currentStepIndex].description)
+                    .font(.body)
+                    .padding()
+                    .multilineTextAlignment(.center)
+
+                Spacer()
+
+                Button(action: {
+                    if viewModel.isLastStep {
+                        onCompletion()
+                    } else {
+                        viewModel.nextStep()
+                    }
+                }) {
+                    Text(viewModel.isLastStep ? "Get Started" : "Continue")
+                        .font(.headline)
+                        .padding()
+                        .frame(maxWidth: .infinity)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .padding(.horizontal)
+                }
+            }
+            .padding()
+        }
+    }
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView(onCompletion: { })
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
